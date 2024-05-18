5466432da780596f372096a66dfc900b78fb2afc
kySuga <kyle@masuga.com>
2024-05-17 21:46:11 -0400

    Remove unused assets, update readme file.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Welcome to my portfolio repository.
 
-I’m currently keeping it simple, cause I like simplicity.
+I’m currently keeping the site simple, cause I like simplicity.
+
+I could've easily built out what I currently have with vanilla HTML, CSS, and JavaScript, but chose to spin it up as a Next.js project to try something different.
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
 import { Manrope } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
 
-// const inter = Inter({ subsets: ["latin"] });
 const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
